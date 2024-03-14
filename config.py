cec44316fbe980568732e1d670f2bcd481c6c8a0
ㅤㅤㅤㅤ <133571161+HVGaruda@users.noreply.github.com>
2024-03-14 08:16:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/PIRATE303/TitanXMusic",
+    "https://github.com/HVGaruda/Musico",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
